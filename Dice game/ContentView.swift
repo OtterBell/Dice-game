63c56e95443dbab3693e607929945a2d6c507e8b
OtterBell <r08625028@ntu.edu.tw>
2025-05-01 06:58:19 +0800

    Initial Commit

---
+++ Dice game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dice game
+//
+//  Created by Mac11 on 2025/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dice game/Dice_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_gameApp.swift
+//  Dice game
+//
+//  Created by Mac11 on 2025/5/1.
+//
+
+import SwiftUI
+
+@main
+struct Dice_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
